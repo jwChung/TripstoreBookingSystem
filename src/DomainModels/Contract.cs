6fea9a4f2d9a7cee42286f49f757a7373dd610e1
Jin-Wook Chung <jwchung@hotmail.com>
2019-06-27 22:43:29 +0900

    1. Conract 클래스 구현

---
+++ src/DomainModels/Contract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tripstore.DomainModels
+{
+    public class Contract
+    {
+        public Contract(string name, string mobileNumber)
+        {
+            if (name == "")
+                throw new ArgumentException();
+
+            var numberParts = mobileNumber.Split('-');
+            if (numberParts.Length != 3 
+                || numberParts[0] != "010"
+                || numberParts[1].Length != 4
+                || numberParts[2].Length != 4)
+                throw new ArgumentException();
+
+            this.Name = name;
+            this.MobileNumber = mobileNumber;
+        }
+
+        public string Name { get; }
+
+        public string MobileNumber { get; set; }
+    }
+}
--- test/Tests/DomainModels/ContractTest.cs
+++ test/Tests/DomainModels/ContractTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Tripstore.DomainModels
 {
@@ -8,8 +9,73 @@ namespace Tripstore.DomainModels
         public void ConstructorWithInvalidNameThrows()
         {
             // Arrange
+            var invalidName = "";
+            var dummyMobileNumber = "010-1234-1234";
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new Contract(invalidName, dummyMobileNumber));
+        }
+
+        //[Fact]
+        //public void ConstructorWithValidNameCorrectlyInitializesName()
+        //{
+        //    // Arrange
+        //    var validName = "정진욱";
+        //    var dummyMobileNumber = "010-1234-1234";
+
+        //    // Act
+        //    var sut = new Contract(validName, dummyMobileNumber);
+
+        //    // Assert
+        //    string actual = sut.Name;
+        //    Assert.Equal(validName, actual);
+        //}
+
+        [Theory]
+        [InlineData("정진욱")]
+        [InlineData("홍길동")]
+        public void ConstructorWithValidNameCorrectlyInitializesName(string validName)
+        {
+            // Arrange
+            var dummyMobileNumber = "010-1234-1234";
+
             // Act
+            var sut = new Contract(validName, dummyMobileNumber);
+
             // Assert
+            string actual = sut.Name;
+            Assert.Equal(validName, actual);
+        }
+
+        [Theory]
+        [InlineData("000-1234-1234")] // 010 아닌 시작번호
+        [InlineData("010-1234")]
+        [InlineData("011-1234-1234")]
+        [InlineData("010-12345-1234")]
+        [InlineData("010-1234-12346")]
+        public void ConstructorWithInvalidMobileNumberThrows(string invalidMobileNumber)
+        {
+            // Arrange
+            var dummyName = "정진욱";
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new Contract(dummyName, invalidMobileNumber));
+        }
+
+        [Theory]
+        [InlineData("010-1234-1234")]
+        [InlineData("010-1235-1111")]
+        public void ConstructorWithValidMobileNumberCorrectlyInitializesMobileNumber(string validMobileNumber)
+        {
+            // Arrange
+            var dummyName = "정진욱";
+
+            // Act
+            var sut = new Contract(dummyName, validMobileNumber);
+
+            // Assert
+            string actual = sut.MobileNumber;
+            Assert.Equal(validMobileNumber, actual);
         }
     }
 }
