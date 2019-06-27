9c7def91afb429fef83608abfba9b3fa26cb63df
Jin-Wook Chung <jwchung@hotmail.com>
2019-06-27 21:39:11 +0900

    0. Contract 예제 준비 완료

---
+++ test/Tests/DomainModels/ContractTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Tripstore.DomainModels
+{
+    public class ContractTest
+    {
+        [Fact]
+        public void ConstructorWithInvalidNameThrows()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
